f09007390cc0403f073887179912558293511d2c
mishaszu <mishaszu@gmail.com>
2024-09-19 14:16:10 +0200

    add for lsp leptos

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -66,6 +66,24 @@ return {
         },
       })
 
+      lspconfig.rust_analyzer.setup({
+        -- Other Configs ...
+        settings = {
+          ["rust-analyzer"] = {
+            -- Other Settings ...
+            procMacro = {
+              ignored = {
+                leptos_macro = {
+                  -- optional: --
+                  "component",
+                  "server",
+                },
+              },
+            },
+          },
+        },
+      })
+
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(ev)
