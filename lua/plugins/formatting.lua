bd5e0bcc459680bd8d5d029a52a1e785f3444719
Michal Szulczewski <michal@beyondplay.io>
2024-10-11 10:14:17 +0200

    update formatters

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -15,7 +15,7 @@ return {
         html = { "prettier" },
         json = { "prettier" },
         yaml = { "prettier" },
-        markdown = { "prettier" },
+        markdown = { "mdformat" },
         graphql = { "prettier" },
         liquid = { "prettier" },
         lua = { "stylua" },
